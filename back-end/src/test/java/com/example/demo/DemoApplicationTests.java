0e959c29eab1b34e3fbe39e6c0bd012cc166f36c
B-Khouloud <mskhouloudbouziri@gmail.com>
2023-02-28 18:05:39 +0100

    change class name

--- back-end/src/test/java/com/example/demo/DemoApplicationTests.java
+++ back-end/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backend;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackEndApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
